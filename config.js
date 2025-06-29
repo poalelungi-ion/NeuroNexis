e67e599da254f4fdd097339bd73b77c4a768c39e
poalelungi-ion <70956168+poalelungi-ion@users.noreply.github.com>
2025-06-29 10:43:38 +0200

    udate

---
+++ config.js
@@ -0,0 +1,6 @@
+window.config = {
+    GOOGLE_CLIENT_ID: '906465572275-hanrq89dvi0a8bpf2benqdq7quu3njtc.apps.googleusercontent.com',
+    GOOGLE_CLIENT_SECRET: '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',
+    XAI_API_KEY: 'xai-R1TV2KLRbfrabO8Jq7T83GIxf1USctByDSRH6Z0fVzRva7FJ7XnWkWSbFPqgRsmBSK8awxjrHg4S3l1F',
+    FACEBOOK_APP_ID: '1024529488747243'
+};
